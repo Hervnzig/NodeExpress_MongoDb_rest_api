fcc7365270bce4117cdbfeb08d66c4fa443c13cb
Herve <hervnzig1@gmail.com>
2020-10-15 09:28:51 +0200

    GET tests implemented

--- test/blog.js
+++ test/blog.js
@@ -24,25 +24,62 @@ describe("Blogs API", () => {
         });
     });
 
-    it("It should get", (done) => {
+    it("It should NOT get all the blogs", (done) => {
       chai
         .request(server)
-        .get("/")
+        .get("/blog")
         .end((err, response) => {
-          response.should.have.status(200);
-          response.body.should.have
-            .property("message")
-            .eql(
-              "Welcome to my login add /user/login-user, for signup add a user/register-user/"
-            );
+          response.should.have.status(404);
           done();
         });
     });
+
+    // it("It should get the root content", (done) => {
+    //   chai
+    //     .request(server)
+    //     .get("/")
+    //     .end((err, response) => {
+    //       response.should.have.status(200);
+    //       response.body.should.have
+    //         .property("message")
+    //         .eql(
+    //           "Welcome to my login add /user/login-user, for signup add a user/register-user/"
+    //         );
+    //       done();
+    //     });
+    // });
   });
 
   /**
    * Test the GET {by id} route
    */
+
+  describe("GET /blogs/blogID", () => {
+    it("It should get a single blog by ID", (done) => {
+      const blogID = "5f77501df933490004892b7f";
+      chai
+        .request(server)
+        .get("/blogs/" + blogID)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("object");
+          done();
+        });
+    });
+
+    // Test to get single student record
+    it("It should not get a single blog record", (done) => {
+      const blogID = 5;
+      chai
+        .request(server)
+        .get(`/blogs/${blogID}`)
+        .end((err, response) => {
+          response.should.have.status(404);
+          done();
+        });
+    });
+  });
+
   /**
    * Test the POST route
    */
